7f2c7816321d784bb5c33a2df7c66f1f365dcdb9
seva <sevaussr@gmail.com>
2021-11-23 12:26:58 +0300

    Прилажение на реакте с марвел апи

--- marvel_starte/src/services/MarvelService.js
+++ marvel_starte/src/services/MarvelService.js
@@ -1,26 +1,39 @@
-
-
 class MarvelService {
-
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=941c610a11de4f59123dbe349500958e'
+    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+    _apiKey = 'apikey=941c610a11de4f59123dbe349500958e';
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
-
+    
         if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);  
-        } 
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
         return await res.json();
     }
 
-    //Получение всех персонажей
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
-    //Получение одного персрнажа
-      getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+            comics: char.comics.items
+        }
     }
 }
 
